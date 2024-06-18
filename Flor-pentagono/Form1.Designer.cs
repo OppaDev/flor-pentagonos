2612d72b4d00e6340799eee287b61929d8d76667
Cristian Acalo <cristianchoacalo@gmail.com>
2024-06-17 19:57:40 -0500

    Desabilitación de cambio de tamaño de ventana

--- Flor-pentagono/Form1.Designer.cs
+++ Flor-pentagono/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pnlColorSelected = new System.Windows.Forms.Panel();
             this.btnColor = new System.Windows.Forms.Button();
             this.txtLado = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -39,7 +40,6 @@
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.picCanvas = new System.Windows.Forms.PictureBox();
             this.darkMode = new System.Windows.Forms.Label();
-            this.pnlColorSelected = new System.Windows.Forms.Panel();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -54,22 +54,31 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.groupBox1.Location = new System.Drawing.Point(11, 88);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Location = new System.Drawing.Point(15, 108);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox1.Size = new System.Drawing.Size(171, 141);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(228, 174);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Entradas";
             // 
+            // pnlColorSelected
+            // 
+            this.pnlColorSelected.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlColorSelected.Location = new System.Drawing.Point(171, 122);
+            this.pnlColorSelected.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlColorSelected.Name = "pnlColorSelected";
+            this.pnlColorSelected.Size = new System.Drawing.Size(38, 35);
+            this.pnlColorSelected.TabIndex = 4;
+            // 
             // btnColor
             // 
             this.btnColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnColor.Location = new System.Drawing.Point(8, 99);
-            this.btnColor.Margin = new System.Windows.Forms.Padding(2);
+            this.btnColor.Location = new System.Drawing.Point(11, 122);
+            this.btnColor.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnColor.Name = "btnColor";
-            this.btnColor.Size = new System.Drawing.Size(115, 29);
+            this.btnColor.Size = new System.Drawing.Size(153, 36);
             this.btnColor.TabIndex = 3;
             this.btnColor.Text = "Color";
             this.btnColor.UseVisualStyleBackColor = true;
@@ -78,18 +87,17 @@
             // txtLado
             // 
             this.txtLado.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.txtLado.Location = new System.Drawing.Point(68, 45);
-            this.txtLado.Margin = new System.Windows.Forms.Padding(2);
+            this.txtLado.Location = new System.Drawing.Point(91, 55);
+            this.txtLado.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtLado.Name = "txtLado";
-            this.txtLado.Size = new System.Drawing.Size(89, 26);
+            this.txtLado.Size = new System.Drawing.Size(117, 26);
             this.txtLado.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label1.Location = new System.Drawing.Point(4, 48);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(5, 59);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(69, 20);
             this.label1.TabIndex = 0;
@@ -102,21 +110,21 @@
             this.groupBox2.Controls.Add(this.btnDibujar);
             this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox2.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.groupBox2.Location = new System.Drawing.Point(11, 255);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Location = new System.Drawing.Point(15, 314);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox2.Size = new System.Drawing.Size(171, 185);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Size = new System.Drawing.Size(228, 228);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Botones";
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(42, 134);
-            this.btnSalir.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSalir.Location = new System.Drawing.Point(56, 165);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(88, 29);
+            this.btnSalir.Size = new System.Drawing.Size(117, 36);
             this.btnSalir.TabIndex = 2;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
@@ -124,10 +132,10 @@
             // 
             // btnLimpiar
             // 
-            this.btnLimpiar.Location = new System.Drawing.Point(42, 85);
-            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLimpiar.Location = new System.Drawing.Point(56, 105);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLimpiar.Name = "btnLimpiar";
-            this.btnLimpiar.Size = new System.Drawing.Size(88, 29);
+            this.btnLimpiar.Size = new System.Drawing.Size(117, 36);
             this.btnLimpiar.TabIndex = 0;
             this.btnLimpiar.Text = "Limpiar";
             this.btnLimpiar.UseVisualStyleBackColor = true;
@@ -136,10 +144,10 @@
             // btnDibujar
             // 
             this.btnDibujar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDibujar.Location = new System.Drawing.Point(42, 35);
-            this.btnDibujar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDibujar.Location = new System.Drawing.Point(56, 43);
+            this.btnDibujar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnDibujar.Name = "btnDibujar";
-            this.btnDibujar.Size = new System.Drawing.Size(88, 29);
+            this.btnDibujar.Size = new System.Drawing.Size(117, 36);
             this.btnDibujar.TabIndex = 1;
             this.btnDibujar.Text = "Dibujar";
             this.btnDibujar.UseVisualStyleBackColor = true;
@@ -150,21 +158,21 @@
             this.groupBox3.Controls.Add(this.picCanvas);
             this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox3.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.groupBox3.Location = new System.Drawing.Point(195, 10);
-            this.groupBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Location = new System.Drawing.Point(260, 12);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox3.Size = new System.Drawing.Size(592, 514);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox3.Size = new System.Drawing.Size(789, 633);
             this.groupBox3.TabIndex = 3;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Grafico";
             // 
             // picCanvas
             // 
-            this.picCanvas.Location = new System.Drawing.Point(4, 23);
-            this.picCanvas.Margin = new System.Windows.Forms.Padding(2);
+            this.picCanvas.Location = new System.Drawing.Point(5, 28);
+            this.picCanvas.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.picCanvas.Name = "picCanvas";
-            this.picCanvas.Size = new System.Drawing.Size(584, 487);
+            this.picCanvas.Size = new System.Drawing.Size(779, 599);
             this.picCanvas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picCanvas.TabIndex = 0;
             this.picCanvas.TabStop = false;
@@ -176,32 +184,27 @@
             this.darkMode.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.darkMode.ForeColor = System.Drawing.SystemColors.Highlight;
             this.darkMode.Location = new System.Drawing.Point(1, -1);
+            this.darkMode.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.darkMode.Name = "darkMode";
             this.darkMode.Size = new System.Drawing.Size(78, 55);
             this.darkMode.TabIndex = 5;
             this.darkMode.Text = "🌙";
             this.darkMode.Click += new System.EventHandler(this.label2_Click);
             // 
-            // pnlColorSelected
-            // 
-            this.pnlColorSelected.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pnlColorSelected.Location = new System.Drawing.Point(128, 99);
-            this.pnlColorSelected.Name = "pnlColorSelected";
-            this.pnlColorSelected.Size = new System.Drawing.Size(29, 29);
-            this.pnlColorSelected.TabIndex = 4;
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.ClientSize = new System.Drawing.Size(796, 534);
+            this.ClientSize = new System.Drawing.Size(1061, 657);
             this.Controls.Add(this.darkMode);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Flor de Péntagonos";
